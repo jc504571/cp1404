cf68cd6982855b88374134b68e0a2e789d0ac279
jc504571 <navjotkaur7@my.jcu.edu.au>
2019-09-04 13:42:24 +1000

    Add file

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,2 @@
+def main():
+   number_of_quick_picks = int(input(""))
---
+++ prac_05/hex_colours.py
@@ -0,0 +1,6 @@
+"""
+CP1404 Practical hexadecimal colour code
+"""
+colour_codes = {"AliceBlue"}
+colour_name = input("Enter a colour_name:")
+While colour_name != "":
